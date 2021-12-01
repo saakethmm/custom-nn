dd3b859e27fbc094c1a89a1b9a4daca59b8a6125
Eli Smith <googolplex8@yahoo.com>
2021-12-01 15:25:43 -0500

    Activation Layer

--- ActivationFunction.py
+++ ActivationFunction.py
@@ -1,15 +1,57 @@
 import numpy as np
+from Layer import Layer
 
-class ActivationFunction:
+
+class ActivationFunction(Layer):
     def __init__(self, type):
-        super(ActivationFunction, self).__init__()
-        self.type = None
+        Layer.__init__(self)
+        # super(ActivationFunction, self).__init__()
+        self.type = type
 
-    def forwardPop(self):
-        return 0
+    def forwardPop(self, input_data):
+        self.input = input_data
+        if type == "tan":
+            self.output = self.tanh(self.input)
+        elif type == "sigmoid":
+            self.output = self.sigmoid(self.input)
+        elif type == "relu":
+            self.output = self.ReLU(self.input)
+        elif type == "leaky":
+            self.output = self.leaky_ReLU(self.input)
+        return self.output
 
-    def backProp(self):
-        return 0
+    def backProp(self, output_error):
+        if type == "tan":
+            return self.tanh_prime(self.input)*output_error
+        elif type == "sigmoid":
+            return self.tanh_prime(self.input)*output_error
+        elif type == "relu":
+            return self.ReLU_prime(self.input)*output_error
+        elif type == "leaky":
+            return self.leaky_ReLU_prime(self.input)*output_error
 
-    def tanh(self):
-        return 0
+    def tanh(self, x):
+        return np.tanh(x)
+
+    def tanh_prime(self, x):
+        return 1-np.tanh(x)**2
+
+    def sigmoid(self, x):
+        return 1/(1 + np.exp(-x))
+
+    def sigmoid_prime(self, x):
+        return np.exp(-x)/(1 + np.exp(-x))**2
+
+    def ReLU(self, x):
+        return x * (x > 0)
+
+    def ReLU_prime(self, x):
+        return 1. * (x > 0)
+
+    def leaky_ReLU(self, x):
+        alpha = 0.1
+        return np.max(alpha*x, x)
+
+    def leaky_ReLU_prime(self, x):
+        alpha = 0.1
+        return 1 if x>=0 else alpha
--- Layer.py
+++ Layer.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+
 class Layer:
     def __init__(self):
         self.input = None
         self.output = None
 
-    def forwardPop(self):
+    def forwardPop(self, input):
         return 0
 
-    def backProp(self):
+    def backProp(self, output_error):
         return 0
